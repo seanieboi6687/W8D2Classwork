6c7d63f9357f7c357e5d68e5f7bbd8219af17b51
Sean <pca.sean.jeun@gmail.com>
2023-08-15 15:11:49 -0400

    myMap sort of built?

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -3,12 +3,9 @@ const callback = function(num) {
  };
 
 Array.prototype.myEach = function(callback){
-    // let result=[]
     this.forEach((ele)=>{
-        // result.push(callback(ele));
         console.log(callback(ele));
     });
-    // return result;
 };
 
 ;
@@ -16,3 +13,13 @@ Array.prototype.myEach = function(callback){
 console.log([0, 1, 2].myEach(callback));
 
 
+Array.prototype.myMap = function(callback){
+    let result = []
+    this.forEach((ele) => {
+        result.push(callback(ele));
+    });
+
+    return result;
+};
+
+console.log([2, 3, 4, 5].myEach(callback));
--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
@@ -1,1 +1,3 @@
-// Your code here
+Array.prototype.bubbleSort = function(){
+
+};
