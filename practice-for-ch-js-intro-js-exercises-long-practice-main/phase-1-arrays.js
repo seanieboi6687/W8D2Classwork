fc00357af167fc7a3c88492aeff6f787ff7cb7c3
Sean <pca.sean.jeun@gmail.com>
2023-08-15 14:30:46 -0400

    transpose function built

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -30,3 +30,19 @@ Array.prototype.twoSum = function(){
 };
 
 console.log([-1, 0, 2, -2, 1].twoSum());
+
+Array.prototype.transpose = function(){
+    let result = [];
+    
+    this.forEach((ele1, idx1)=>{
+        let subarr = []
+        this.forEach((ele2, idx2) => {
+            subarr.push(this[idx2][idx1]);
+        });
+        result.push(subarr);
+    });
+
+    return result;
+};
+
+console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose());
