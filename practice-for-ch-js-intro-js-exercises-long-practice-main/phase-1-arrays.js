e9a24b99877a61a124bfa0f03b61887c3a6a160e
Sean <pca.sean.jeun@gmail.com>
2023-08-15 14:12:47 -0400

    Array.protoype.uniq function built

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,12 @@
-// Your code here
+Array.prototype.uniq = function (){
+    let result = []
+    this.forEach((ele) => { 
+        if (!result.includes(ele)) {
+            result.push(ele)
+        };
+    });
+
+    return result;
+};
+
+console.log([1, 2, 2, 3, 3, 3].uniq())
