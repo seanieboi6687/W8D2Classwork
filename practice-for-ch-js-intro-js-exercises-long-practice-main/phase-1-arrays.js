c99b357ed48665ef1fed6bf25442f9be17158fa8
elevenstand7 <elevenstand7@gmail.com>
2023-08-15 14:23:05 -0400

    two sum

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,6 +1,6 @@
 Array.prototype.uniq = function (){
     let result = []
-    this.forEach((ele) => { 
+    this.forEach((ele) => {
         if (!result.includes(ele)) {
             result.push(ele)
         };
@@ -10,3 +10,23 @@ Array.prototype.uniq = function (){
 };
 
 console.log([1, 2, 2, 3, 3, 3].uniq())
+
+Array.prototype.twoSum = function(){
+    let result = [];
+    this.forEach((ele1, idx1)=>{
+        let pair=[];
+        this.forEach((ele2, idx2)=>{
+            if (idx2>idx1){
+                if (ele1+ele2===0){
+                   let pair = [idx1,idx2];
+                   result.push(pair);
+                }
+            }
+
+        });
+    });
+    return result;
+
+};
+
+console.log([-1, 0, 2, -2, 1].twoSum());
